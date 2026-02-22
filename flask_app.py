9ba978b703317c37316c27ce582284a32a619f8f
Harry <harry92@smis.ac.jp>
2026-02-22 13:17:03 +0000

    index.html and base.html

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,8 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Marine Conservation Website'
+def root():
+    return render_template("index.html")
 
